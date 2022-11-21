6a90809150cfe2e5cb1db82cbbeccd403d86e62c
Benjamin Cable <cable.benjamin@protonmail.com>
2022-11-21 18:54:56 +0000

    fix: better docs

--- preload/_init.zsh
+++ preload/_init.zsh
@@ -1,7 +1,7 @@
 _dbg() { if (( ${+VERBOSE_STARTUP} )); then echo $@; fi } # conditional debug print
 _locate() { find "$0" -type f ! -wholename "$0" } 	  # locate files in modules.
 _load() { [ -f $1 ] && source $1 }		          # source a file.
-_is_zsh_file() { [ "${1##*.}" = "zsh" ] }
+_is_zsh_file() { [ "${1##*.}" = "zsh" ] }		  # check if file is a zsh file.
 
 # This one will spit out a warning, annoying the user.
 warn_is_installed() {
@@ -20,6 +20,7 @@ is_installed() {
         return 1
 }
 
+# This one will fail silently, without warning.
 is_macos() {
 	# never gonna use windows so I don't need to be smart!
 	if [ "$(uname 2> /dev/null)" != "Linux" ]; then
@@ -28,6 +29,7 @@ is_macos() {
 	return 1
 }
 
+# This one will fail silently, without warning.
 is_linux() {
 	if [ "$(uname 2> /dev/null)" = "Linux" ]; then
 		return 0
