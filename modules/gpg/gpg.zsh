070d0259fd97d65ce3a38701643b9b432c8f16fb
Benjamin Cable <cable.benjamin@protonmail.com>
2023-04-06 12:03:21 +0100

    fix: ensure gpg options are always set

--- modules/gpg/gpg.zsh
+++ modules/gpg/gpg.zsh
@@ -1,1 +1,25 @@
 export GPG_TTY=$(tty)
+
+# GPG_AGENT_CONF is the path to the gpg-agent.conf file.
+GPG_AGENT_CONF="${HOME}/.gnupg/gpg-agent.conf"
+
+# PINENTRY_PROGRAM is the path to the pinentry program.
+PINENTRY_PROGRAM="/usr/bin/pinentry-qt"
+
+
+# Ensure that the gpg-agent.conf file has the correct options enabled.
+OPTIONS=(
+    "pinentry-program ${PINENTRY_PROGRAM}"
+    "enable-ssh-support"
+    "default-cache-ttl 86400"
+    "max-cache-ttl 86400"
+)
+
+for OPTION in "${OPTIONS[@]}"; do
+    if ! grep -q "^${OPTION}$" "${GPG_AGENT_CONF}"; then
+        echo "Appending '${OPTION}' to ${GPG_AGENT_CONF} ..."
+        echo "${OPTION}" >> "${GPG_AGENT_CONF}"
+    else
+        _dbg "The '${OPTION}' option is already present in ${GPG_AGENT_CONF}."
+    fi
+done
