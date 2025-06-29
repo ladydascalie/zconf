6ff0967fa75d61b0a15ab1d41d7d5ae7756deb09
Benjamin Cable <cable.benjamin@protonmail.com>
2025-06-30 00:49:03 +0100

    remove nvm module script

--- modules/nvm/nvm.zsh
+++
@@ -1,5 +0,0 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # load nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # load nvm bash_completion
-
-warn_is_installed nvm
