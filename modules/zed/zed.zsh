7d764aa70af63e0e8a6ca264ffa4cd827d84fa32
Benjamin Cable <cable.benjamin@protonmail.com>
2025-09-11 12:37:05 +0100

    chore: save!

--- modules/zed/zed.zsh
+++ modules/zed/zed.zsh
@@ -1,16 +1,19 @@
 local dir=$(dirname $0)
-local config_in_place=$dir/zed
+local config_in_place=$dir/zed-config-folder
 local config_symlink=$HOME/.config/zed
 
 # ensure the destination directory exists
 mkdir -p $(dirname $config_symlink)
 
-# check file exists
-if [ -f $config_symlink ]; then
-        # if it does, all good.
-        _dbg "module(zed) ~> $config_symlink already exists, nothing to do."
+# check if the symlink exists and points to the correct target
+if [ -L $config_symlink ] && [ "$(readlink $config_symlink)" = "$config_in_place" ]; then
+    # if it does, all good.
+    _dbg "module(zed) ~> $config_symlink already exists and points to the correct target, nothing to do."
+elif [ -e $config_symlink ]; then
+    # if a file or directory exists at the symlink location but it's not a symlink
+    _dbg "module(zed) ~> $config_symlink exists but is not a symlink. Manual intervention required."
 else
-        # symlink to the home directory.
-        _dbg "module(zed) ~> symlinking $config_in_place to $config_symlink"
-        ln -s $config_in_place $config_symlink
+    # symlink to the home directory.
+    _dbg "module(zed) ~> symlinking $config_in_place to $config_symlink"
+    ln -s $config_in_place $config_symlink
 fi
